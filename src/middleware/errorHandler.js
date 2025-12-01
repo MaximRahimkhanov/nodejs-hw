3a05156bf3192dc948eda9b8907d97a87924de00
MaximRahimkhanov <zloyigrog@gmail.com>
2025-12-01 22:11:35 +0200

    fix 5&8 tasks

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,15 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
   const isProd = process.env.NODE_ENV === 'production';
+
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
 
   res.status(500).json({
     message: isProd
