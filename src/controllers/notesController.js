fb0658070e210cd203d92d3bc06409efcb4321b0
Max <zloyigrog@gmail.com>
2025-12-13 15:04:51 +0200

    Update notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -32,7 +32,7 @@ export const getAllNotes = async (req, res, next) => {
 
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findById({ _id: noteId, userId: req.user._id });
+  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
 
   if (!note) {
     next(createHttpError(404, 'Note not found'));
