b897f5e472fbbd024a4d6e5a14671c0294fd43ff
MaximRahimkhanov <zloyigrog@gmail.com>
2025-12-19 19:15:39 +0200

    fix sendMail

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,8 @@ import jwt from 'jsonwebtoken';
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
+// import { sendEmail } from '../utils/sendEmail.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
