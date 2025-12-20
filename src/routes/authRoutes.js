b06099661c5743c37c98155acac6cc6d8c2b634b
MaximRahimkhanov <zloyigrog@gmail.com>
2025-12-20 03:02:17 +0200

    change file route patch

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -14,9 +14,6 @@ import {
   requestResetEmail,
   resetPassword,
 } from '../controllers/authController.js';
-import { authenticate } from '../middleware/authenticate.js';
-import { upload } from '../middleware/multer.js';
-import { updateUserAvatar } from '../controllers/userController.js';
 
 const router = Router();
 
@@ -38,13 +35,6 @@ router.post(
   '/auth/reset-password',
   celebrate(resetPasswordSchema),
   resetPassword,
-);
-
-router.patch(
-  '/users/me/avatar',
-  authenticate,
-  upload.single('avatar'),
-  updateUserAvatar,
 );
 
 export default router;
